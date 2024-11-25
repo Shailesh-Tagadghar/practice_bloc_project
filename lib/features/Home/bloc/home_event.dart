1473f4191e16063acd755faf95ca26cd24f72c7f
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-11-25 10:38:54 +0530

    event.dart -- here we add events that are going to accord while dealing with home screen

--- lib/features/Home/bloc/home_event.dart
+++ lib/features/Home/bloc/home_event.dart
@@ -10,3 +10,6 @@ class HomeWishlistButtonEvent extends HomeEvent {}
 class HomeCartNavigateEvent extends HomeEvent {}
 
 class HomeWishlistNavigateEvent extends HomeEvent {}
+
+
+// In this event -- will add event like botton_click and navigation like this
