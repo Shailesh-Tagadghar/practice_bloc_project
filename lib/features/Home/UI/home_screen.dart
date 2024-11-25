e113ce9a8002c333500d7e49c732fa76232eed24
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-11-25 11:02:07 +0530

    bloc.dart-- will write logic like navigate the page or click on icon

--- lib/features/Home/UI/home_screen.dart
+++ lib/features/Home/UI/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practice_bloc_project/features/Home/bloc/home_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,20 +10,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final HomeBloc homeBloc = HomeBloc();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: const Text(
-          'Grocery List',
-          style: TextStyle(
-            color: Colors.white,
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      // buildWhen: (previous, current) {},
+      // listenWhen: (previous, current) {},
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blueAccent,
+            title: const Text(
+              'Grocery List',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            toolbarHeight: 60,
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    homeBloc.add(HomeWishlistButtonEvent());
+                  },
+                  icon: const Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    homeBloc.add(HomeCartButtonEvent());
+                  },
+                  icon: const Icon(
+                    Icons.shopping_cart_outlined,
+                    color: Colors.white,
+                  ))
+            ],
           ),
-        ),
-        centerTitle: true,
-        toolbarHeight: 60,
-      ),
+        );
+      },
     );
   }
 }
--- lib/features/Home/bloc/home_bloc.dart
+++ lib/features/Home/bloc/home_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -6,8 +8,21 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomeWishlistButtonEvent>(homeWishlistButtonEvent);
+
+    on<HomeCartButtonEvent>(homeCartButtonEvent);
+  }
+
+  FutureOr<void> homeWishlistButtonEvent(
+      HomeWishlistButtonEvent event, Emitter<HomeState> emit) {
+    print('Wishlist icon clicked');
+  }
+
+  FutureOr<void> homeCartButtonEvent(
+      HomeCartButtonEvent event, Emitter<HomeState> emit) {
+    print('Cart icon clicked');
   }
 }
+
+
+//in this bloc -- will write logic like navigate the page or click on icon 
