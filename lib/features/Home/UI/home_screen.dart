efb4c86ba3f2d813f0b5f78694c532fd5b8e7ddd
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-11-25 15:05:07 +0530

    UI updated to get Static data

--- lib/features/Home/UI/home_screen.dart
+++ lib/features/Home/UI/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice_bloc_project/features/Cart/UI/cart_screen.dart';
+import 'package:practice_bloc_project/features/Home/UI/models/product_data_model.dart';
 import 'package:practice_bloc_project/features/Home/bloc/home_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:practice_bloc_project/features/Wishlist/UI/wishlist_screen.dart';
@@ -19,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   final HomeBloc homeBloc = HomeBloc();
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<HomeBloc, HomeState>(
@@ -43,6 +45,8 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             );
           case const (HomeLoadingSuccessState):
+            final ProductDataModel productDataModel;
+            final successState = state as HomeLoadingSuccessState;
             return Scaffold(
               appBar: AppBar(
                 backgroundColor: Colors.blueAccent,
@@ -52,7 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     color: Colors.white,
                   ),
                 ),
-                toolbarHeight: 60,
+                toolbarHeight: 70,
                 actions: [
                   IconButton(
                       onPressed: () {
@@ -72,8 +76,84 @@ class _HomeScreenState extends State<HomeScreen> {
                       ))
                 ],
               ),
+              body: ListView.builder(
+                  padding: const EdgeInsets.only(
+                    top: 12,
+                    bottom: 12,
+                  ),
+                  itemCount: successState.products.length,
+                  itemBuilder: (context, index) {
+                    final productDataModel = successState.products[index];
+                    return Container(
+                      margin: const EdgeInsets.only(
+                        left: 8,
+                        right: 8,
+                        top: 8,
+                        bottom: 8,
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(
+                                        productDataModel.imageUrl))),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Name : ${productDataModel.name}',
+                            style: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Price : \$ ${productDataModel.price.toString()}',
+                                style: const TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.favorite_border_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.shopping_cart_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
             );
-          case HomeErrorState:
+          case const (HomeErrorState):
             return const Scaffold(
               body: Center(
                 child: Text('Something went wrong'),
